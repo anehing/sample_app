d46e12b464b929582a1628d3d40b7405869d3728
anehing <anehing@gmail.com>
2014-03-04 22:54:16 +0800

    part

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,16 @@
 class UsersController < ApplicationController
+  before_action :signed_in_user, only: [:edit, :update]
+  before_action :correct_user,   only: [:edit, :update]
+  
   def new
      @user = User.new
   end
   def show
       @user = User.find(params[:id])
   end
-  
+  def edit
+      # @user = User.find(params[:id])
+  end
   def create
     @user = User.new(user_params)
     if @user.save
@@ -16,10 +21,29 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
-  
+  def update
+      # @user = User.find(params[:id])
+      if @user.update_attributes(user_params)
+        # Handle a successful update.
+        flash[:success] = "Profile updated"
+        redirect_to @user
+      else
+        render 'edit'
+      end
+  end
+    
   private
+  
     def user_params
       params.require(:user).permit(:name, :email, :password,:password_confirmation)
     end
- 
+    
+    def signed_in_user
+      redirect_to signin_url, notice: "Please sign in." unless signed_in?
+    end  
+    
+    def correct_user
+       @user = User.find(params[:id])
+       redirect_to(root_path) unless current_user?(@user)
+    end
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -16,12 +16,15 @@ module SessionsHelper
       @current_user = user
   end
   def current_user
-    remember_token = User.hash(cookies[:remember_token])
-    @current_user ||= User.find_by(remember_token: remember_token)
+      remember_token = User.hash(cookies[:remember_token])
+      @current_user ||= User.find_by(remember_token: remember_token)
   end
   def signed_in?
-    !current_user.nil?
+      !current_user.nil?
   end
-
+  def current_user?(user)
+      user == current_user
+  end
+  
 end
 
